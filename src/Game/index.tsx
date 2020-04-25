42f15b4d29004532c0622aa3eb08525ce0614c46
onichandame <zxinmyth@gmail.com>
2020-04-25 14:31:20 +0000

    staged. trying unreal

--- src/Game/index.tsx
+++ src/Game/index.tsx
@@ -2,17 +2,13 @@ import React, {FC,useState, useRef,Suspense}  from 'react'
 import {Canvas,useFrame,useLoader} from 'react-three-fiber'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 
-const Box:FC = ()=>{
+import {} from './Model'
+
+const Game:FC = ()=>{
   const gltf=useLoader(GLTFLoader,'/scene.gltf')
   return (
     <primitive object={gltf.scene} dispose={null}/>
   )
 }
 
-export default ()=>(
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10,10,10]}/>
-    <Box />
-  </Canvas>
-)
+export default Game
