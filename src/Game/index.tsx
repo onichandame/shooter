af06487f438142128e0b15eca5ab76bbfb811b1a
onichandame <zxinmyth@gmail.com>
2020-04-25 12:27:40 +0000

    staged. load gltf

--- src/Game/index.tsx
+++ src/Game/index.tsx
@@ -1,28 +1,11 @@
-import React, {FC,useState, useRef}  from 'react'
-import {Canvas,useFrame} from 'react-three-fiber'
+import React, {FC,useState, useRef,Suspense}  from 'react'
+import {Canvas,useFrame,useLoader} from 'react-three-fiber'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 
 const Box:FC = ()=>{
-  const [hover,setHover]=useState<boolean>(false)
-  const [active,setActive]=useState<boolean>(false)
-
-  const mesh=useRef<JSX.IntrinsicElements['mesh']>(null)
-  useFrame(()=>{
-    if(mesh &&mesh.current&&mesh.current.rotateX){
-      mesh.current.rotateX(0.01)
-    }
-  })
+  const gltf=useLoader(GLTFLoader,'/scene.gltf')
   return (
-    <mesh
-      position={[-1.2,0,0]}
-      ref={mesh}
-      scale={active?[1.5,1.5,1.5]:[1,1,1]}
-      onClick={()=>setActive(!active)}
-      onPointerOver={()=>setHover(true)}
-      onPointerOut={()=>setHover(false)}
-    >
-      <boxBufferGeometry attach="geometry" args={[1,1,1]} />
-      <meshStandardMaterial attach="material" color={hover?'hotpink':'orange'}/>
-    </mesh>
+    <primitive object={gltf.scene} dispose={null}/>
   )
 }
 
